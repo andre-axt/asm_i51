683918656dc9bc696400bd68e676019b58672a77
andre-axt <andreaxtbachtold@gmail.com>
2026-06-09 10:33:23 -0300

    Add - find_command function in mnems_i51.rs

--- src/mnems_i51.rs
+++ src/mnems_i51.rs
@@ -1,5 +1,5 @@
 #[derive(Debug, PartialEq)]
-pub enum mnems {
+pub enum Mnems {
     Nop,
     Ajmp(u16),
     Ljmp(u8, u8, u8),
@@ -8,4 +8,19 @@ pub enum mnems {
     Inc_Direct(u16),
     Jbc(u8, u8, u8),
 
+}
+
+pub fn find_command(buffer: &mut String) -> Option<String> {
+    let delims = ['\n', '#', ';'];
+    let pos = buffer.find(delims)?;
+    
+    let prefix = buffer[..pos].to_string();
+    
+    let delims = &buffer[pos..].chars().next().unwrap();
+    let delims_len = delims.len_utf8();
+    let remove_until = pos + delims_len;
+    
+    buffer.drain(..remove_until);
+    
+    Some(prefix)
 }
